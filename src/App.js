8d59cecba13246e98012759f4b800cd65f229539
Jason Libertelli <jason.libertelli@gmail.com>
2024-05-14 16:47:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+ 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my first React App</h1>
+      <h4>Be nice to me! </h4>
+    </div>
+  );
+}
+
+export default App;
